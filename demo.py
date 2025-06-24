bc5b5fb05cc6c6b6d312b92144174fb5f9874b79
michael <mymis168@gmail.com>
2025-06-24 14:41:16 +0800

    udpate v2

--- demo.py
+++ demo.py
@@ -2,11 +2,15 @@ import mysql.connector
 
 #
 print("建立資料庫連線中")
-mysql.connector.connect(
+cnx=mysql.connector.connect(
     host="127.0.0.1",
     port=3306,
     user="dbuser",
-    password="1234"
-
+    password="1234",
+    database="world"
 )
 print("透過連線取得 cursor物件")
+dbcursor=cnx.cursor()
+print("執行 select name from city")
+dbcursor.execute("select name from city")
+
