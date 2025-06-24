3b4ee04813a33bf8f084936257f113f86864e199
michael <mymis168@gmail.com>
2025-06-24 16:17:54 +0800

    insert example

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@ import mysql.connector
 #
 print("建立資料庫連線中")
 cnx=mysql.connector.connect(
-    host="127.0.0.1",
+    host="172.16.16.107",
     port=3306,
     user="dbuser",
     password="1234",
@@ -25,3 +25,6 @@ dbcursor.execute("select name, population from country")
 for (c , p ) in dbcursor:
     print(c, p)
 
+
+dbcursor.close()
+cnx.close()
